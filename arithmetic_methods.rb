fa1346fe2a51041be156efa84b9fec94883475d6
Andrew Nelson <andrew89nelson@gmail.com>
2019-07-23 18:45:18 -0700

    whoop dere it is

---
+++ arithmetic_methods.rb
@@ -0,0 +1,28 @@
+# () are not needed at the end of method calls like in other languages
+# .+ = +
+p 1 + 2
+p 1.+(2)
+
+puts
+
+p 10 - 5
+p 10.-(5)
+p 10.- 5
+
+
+p 3 * 4
+p 3.*(4)
+
+
+puts
+
+p 10 / 5
+p 10./(5)
+p 10.div(5)
+
+
+p 12.0 / 5.0
+puts
+p 10 % 3
+p 10.%(3)
+p 10.modulo(3)
