0a5f7fc9cb90a054fe8724278ce72385ef55e9a3
Andrew Nelson <andrew89nelson@gmail.com>
2019-07-25 17:33:15 -0700

    onda boat wit it

---
+++ assignments_and_reassignments.rb
@@ -0,0 +1,22 @@
+# equations start on the right then reassigns the value of a to be 15
+a = 10
+p a
+# a = a + 5
+# lines 4 & 6 are the same equation
+a += 5
+p a
+
+b = 100
+
+p b - 50
+# b = b - 50
+b -= 50
+# b(value) - number on right(50) then equals
+
+c = 3
+p c
+p c * 4
+# c = c * 4
+p c *= 4
+
+p 3**2
