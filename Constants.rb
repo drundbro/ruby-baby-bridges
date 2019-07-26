9cd8ec8fcd82786dbbdc21279877168979f80362
Andrew Nelson <andrew89nelson@gmail.com>
2019-07-26 16:26:10 -0700

    DONE

---
+++ Constants.rb
@@ -0,0 +1,14 @@
+age = 25
+p age
+
+age = 30
+p age
+# contant needs to be in Capitals
+NAME = "Drew"
+PI = 3.14159
+
+p NAME
+p PI
+# do not change the value of a constant . use a variable instread
+NAME = "Jeff"
+p NAME
